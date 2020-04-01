702457dd8a12f74ea5f530b887e551e02249a13c
alpha-acrobat-9971 <qsmity@gmail.com>
2020-04-01 04:36:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -71,8 +71,9 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined){
     return "Sorry, we don't have a credit card on file for you."
   } else if (typeof cardNumber === 'number'){
+    var total = total()
     cart = []
-    return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is ${total}, which will be charged to the card ${cardNumber}.`
   }
 }
 
