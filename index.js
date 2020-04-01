44dfd012706beb23f251444284527f6eb994746f
alpha-acrobat-9971 <qsmity@gmail.com>
2020-04-01 03:15:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ function removeFromCart(item) {
   for (i = 0; i < cart.length; i++){
     if (item === cart[i].itemName){
       removedItem =+ cart.splice(i,1)
-      console.log(cart)
+      return cart
     } 
   }
   if (removedItem === ""){
